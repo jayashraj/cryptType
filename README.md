ade00970f94452f2d9e508f2aaff3616cda57c77
Jayash Raj Mudbhari <jayashraj@outlook.com>
2021-02-28 02:44:19 +1100

    Does the basics well

--- README.md
+++ README.md
@@ -1,44 +1,19 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app), using the [Redux](https://redux.js.org/) and [Redux Toolkit](https://redux-toolkit.js.org/) template.
+# CryptType
 
-## Available Scripts
+Type your message without worrying about people snooping on your screen.
 
-In the project directory, you can run:
+## Download plain text file
 
-### `npm start`
+Just type `:save` then press space or enter once you are done typing your text.
 
-Runs the app in the development mode.<br />
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+## Copy plain text to clipboard
 
-The page will reload if you make edits.<br />
-You will also see any lint errors in the console.
+Just type `:copy` then press space or enter once you are done typing your text.
 
-### `npm test`
+## Clear all at once
 
-Launches the test runner in the interactive watch mode.<br />
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+Because selecting text doesn't work as intended, you can just enter `:clear` to clear all text
 
-### `npm run build`
+## Dark mode
 
-Builds the app for production to the `build` folder.<br />
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.<br />
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
+Toggle between Dark and Light mode by typing `:dark` and `:light`.
--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,177 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React, { ChangeEvent, KeyboardEvent, useState } from "react";
+import logo from "./logo.svg";
+import styled from "styled-components";
+import "./App.css";
+
+const StyledPage = styled.div`
+  font-size: 30px;
+  padding: 10px;
+  cursor: text;
+  height: 100vh;
+  white-space: pre-wrap;
+
+  input {
+    border: none;
+    width: 0px;
+    font-size: 30px;
+
+    &:focus {
+      outline: none;
+    }
+  }
+  .animated {
+    opacity: 0.87;
+    display: inline-block;
+    position: relative;
+    left: -5px;
+
+    animation: blink-animation 2s steps(5, start) infinite;
+  }
+  @keyframes blink-animation {
+    to {
+      visibility: hidden;
+    }
+  }
+`;
+
+function downloadPlainText(name: string, content: string) {
+  //https://stackoverflow.com/questions/35547835/how-to-create-txt-file-in-javascript
+  var link = document.createElement("a");
+  var file = new Blob([content], { type: "text/plain" });
+  link.href = URL.createObjectURL(file);
+  link.download = name;
+  link.click();
+}
 
 function App() {
+  const [inputValue, changeInputValue] = useState("");
+  const [inputBoxValue, changeInputBoxValue] = useState("");
+  const [randomValue, changeRandomValue] = useState("");
+  const [currentWord, changeCurrentWord] = useState("");
+  const [darkTheme, setDarkTheme] = useState(false);
+  const [copied, setCopied] = useState(false);
+  console.log(inputValue);
+
+  const saveText = (download: boolean) => {
+    let text = inputValue
+      .replace(/:[cC]opy ?/g, "")
+      .replace(/:[dD]ark ?/g, "")
+      .replace(/:[lL]ight ?/g, "")
+      .replace(/:[sS]ave ?/g, "");
+
+    if (download) {
+      let date = Date.now();
+      downloadPlainText(`${date.toString()}.txt`, text);
+    } else {
+      navigator.clipboard.writeText(text).then(() => {});
+    }
+    setCopied(true);
+  };
+  const clearAll = () => {
+    changeInputValue("");
+    changeRandomValue("");
+    changeCurrentWord("");
+  };
+  const addLetter = (letter: string) => {
+    changeCurrentWord("");
+    changeRandomValue(randomValue + letter);
+    changeInputValue(inputValue + letter);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+    <StyledPage
+      className="App"
+      onClick={() => {
+        document.getElementById("input-box")?.focus();
+      }}
+      style={
+        darkTheme
+          ? { backgroundColor: "black", color: "white" }
+          : { backgroundColor: "white", color: "black" }
+      }
+    >
+      {randomValue}
+      <input
+        autoFocus
+        id="input-box"
+        readOnly
+        style={
+          darkTheme
+            ? { backgroundColor: "black", color: "white" }
+            : { backgroundColor: "white", color: "black" }
+        }
+        onKeyDown={(event: KeyboardEvent<HTMLInputElement>) => {
+          event.preventDefault();
+          if (
+            event.key.length > 1 &&
+            event.key !== "Enter" &&
+            event.key !== "Backspace"
+          ) {
+          } else if (event.key.length > 1 && event.key == "Enter") {
+            if (currentWord == ":copy" || currentWord == ":Copy") {
+              saveText(false);
+              addLetter("\n");
+            } else if (currentWord == ":save" || currentWord == ":Save") {
+              saveText(true);
+              addLetter(" ");
+            } else if (currentWord == ":dark" || currentWord == ":Dark") {
+              setDarkTheme(true);
+              addLetter("\n");
+            } else if (currentWord == ":light" || currentWord == ":Light") {
+              setDarkTheme(false);
+              addLetter("\n");
+            } else if (currentWord == ":clear" || currentWord == ":Clear") {
+              clearAll();
+            } else {
+              addLetter("\n");
+            }
+          } else if (event.key.length > 1 && event.key == "Backspace") {
+            changeCurrentWord(currentWord.substr(0, currentWord.length - 1));
+            changeInputValue(inputValue.substr(0, inputValue.length - 1));
+            changeRandomValue(randomValue.substr(0, randomValue.length - 1));
+          } else if (event.key == " ") {
+            if (currentWord == ":copy" || currentWord == ":Copy") {
+              saveText(false);
+              addLetter(" ");
+            } else if (currentWord == ":save" || currentWord == ":Save") {
+              saveText(true);
+              addLetter(" ");
+            } else if (currentWord == ":dark" || currentWord == ":Dark") {
+              setDarkTheme(true);
+              addLetter(" ");
+            } else if (currentWord == ":light" || currentWord == ":Light") {
+              setDarkTheme(false);
+              addLetter(" ");
+            } else if (currentWord == ":clear" || currentWord == ":Clear") {
+              clearAll();
+            } else {
+              addLetter(" ");
+            }
+          } else {
+            changeRandomValue(
+              randomValue + Math.random().toString(36).substring(2, 3)
+            );
+            changeCurrentWord(currentWord + event.key);
+            changeInputValue(inputValue + event.key);
+            setCopied(false);
+          }
+
+          changeInputBoxValue("");
+        }}
+      />
+      <div
+        className="animated"
+        style={
+          copied
+            ? { color: "green" }
+            : darkTheme
+            ? { color: "white" }
+            : { color: "black" }
+        }
+      >
+        |
+      </div>
+    </StyledPage>
   );
 }
 
